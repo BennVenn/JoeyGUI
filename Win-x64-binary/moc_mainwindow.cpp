ba81efd1c620ede177fcd73691ded71894675a05
BennVenn <BennVenn@users.noreply.github.com>
2024-06-15 13:40:48 +1000

    V1.000
    4x speed increase dumping GBA and GB/C

--- Win-x64-binary/moc_mainwindow.cpp
+++ Win-x64-binary/moc_mainwindow.cpp
@@ -51,6 +51,10 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
     "MBC",
     "uint32_t",
     "offset",
+    "requestAndReceiveROMfast",
+    "ReadMode",
+    "baseAddress",
+    "bytesToRead",
     "requestAndReceiveRAM",
     "sendByte",
     "Mode",
@@ -77,13 +81,15 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
     "on_pushButton_UploadSave_clicked",
     "on_pushButton_Detect_clicked",
     "on_pushButton_UploadROM_clicked",
+    "on_pushButton_RTC_clicked",
+    "on_pushButton_SRAMTEST_clicked",
     "on_comboBox_FlashCartType_currentIndexChanged",
     "index",
     "on_pushButton_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[84];
+    uint offsetsAndSizes[96];
     char stringdata0[11];
     char stringdata1[16];
     char stringdata2[1];
@@ -97,35 +103,41 @@ struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
     char stringdata10[4];
     char stringdata11[9];
     char stringdata12[7];
-    char stringdata13[21];
+    char stringdata13[25];
     char stringdata14[9];
-    char stringdata15[5];
-    char stringdata16[8];
-    char stringdata17[5];
+    char stringdata15[12];
+    char stringdata16[12];
+    char stringdata17[21];
     char stringdata18[9];
-    char stringdata19[9];
-    char stringdata20[5];
-    char stringdata21[4];
-    char stringdata22[11];
-    char stringdata23[11];
-    char stringdata24[6];
-    char stringdata25[29];
+    char stringdata19[5];
+    char stringdata20[8];
+    char stringdata21[5];
+    char stringdata22[9];
+    char stringdata23[9];
+    char stringdata24[5];
+    char stringdata25[4];
     char stringdata26[11];
-    char stringdata27[12];
-    char stringdata28[9];
-    char stringdata29[9];
-    char stringdata30[8];
-    char stringdata31[15];
-    char stringdata32[13];
-    char stringdata33[25];
-    char stringdata34[28];
-    char stringdata35[35];
-    char stringdata36[33];
-    char stringdata37[29];
-    char stringdata38[32];
-    char stringdata39[46];
-    char stringdata40[6];
-    char stringdata41[22];
+    char stringdata27[11];
+    char stringdata28[6];
+    char stringdata29[29];
+    char stringdata30[11];
+    char stringdata31[12];
+    char stringdata32[9];
+    char stringdata33[9];
+    char stringdata34[8];
+    char stringdata35[15];
+    char stringdata36[13];
+    char stringdata37[25];
+    char stringdata38[28];
+    char stringdata39[35];
+    char stringdata40[33];
+    char stringdata41[29];
+    char stringdata42[32];
+    char stringdata43[26];
+    char stringdata44[31];
+    char stringdata45[46];
+    char stringdata46[6];
+    char stringdata47[22];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -144,35 +156,41 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(162, 3),  // "MBC"
         QT_MOC_LITERAL(166, 8),  // "uint32_t"
         QT_MOC_LITERAL(175, 6),  // "offset"
-        QT_MOC_LITERAL(182, 20),  // "requestAndReceiveRAM"
-        QT_MOC_LITERAL(203, 8),  // "sendByte"
-        QT_MOC_LITERAL(212, 4),  // "Mode"
-        QT_MOC_LITERAL(217, 7),  // "Address"
-        QT_MOC_LITERAL(225, 4),  // "Data"
-        QT_MOC_LITERAL(230, 8),  // "getBytes"
-        QT_MOC_LITERAL(239, 8),  // "swapBits"
-        QT_MOC_LITERAL(248, 4),  // "temp"
-        QT_MOC_LITERAL(253, 3),  // "QRY"
-        QT_MOC_LITERAL(257, 10),  // "AppendWord"
-        QT_MOC_LITERAL(268, 10),  // "datastring"
-        QT_MOC_LITERAL(279, 5),  // "value"
-        QT_MOC_LITERAL(285, 28),  // "populateComboBoxWithALGFiles"
-        QT_MOC_LITERAL(314, 10),  // "folderPath"
-        QT_MOC_LITERAL(325, 11),  // "readALGFile"
-        QT_MOC_LITERAL(337, 8),  // "filePath"
-        QT_MOC_LITERAL(346, 8),  // "ALGData&"
-        QT_MOC_LITERAL(355, 7),  // "algData"
-        QT_MOC_LITERAL(363, 14),  // "InterrogateCFI"
-        QT_MOC_LITERAL(378, 12),  // "isCFIpresent"
-        QT_MOC_LITERAL(391, 24),  // "on_actionAbout_triggered"
-        QT_MOC_LITERAL(416, 27),  // "on_actionAbout_QT_triggered"
-        QT_MOC_LITERAL(444, 34),  // "on_pushButton_DownloadSave_cl..."
-        QT_MOC_LITERAL(479, 32),  // "on_pushButton_UploadSave_clicked"
-        QT_MOC_LITERAL(512, 28),  // "on_pushButton_Detect_clicked"
-        QT_MOC_LITERAL(541, 31),  // "on_pushButton_UploadROM_clicked"
-        QT_MOC_LITERAL(573, 45),  // "on_comboBox_FlashCartType_cur..."
-        QT_MOC_LITERAL(619, 5),  // "index"
-        QT_MOC_LITERAL(625, 21)   // "on_pushButton_clicked"
+        QT_MOC_LITERAL(182, 24),  // "requestAndReceiveROMfast"
+        QT_MOC_LITERAL(207, 8),  // "ReadMode"
+        QT_MOC_LITERAL(216, 11),  // "baseAddress"
+        QT_MOC_LITERAL(228, 11),  // "bytesToRead"
+        QT_MOC_LITERAL(240, 20),  // "requestAndReceiveRAM"
+        QT_MOC_LITERAL(261, 8),  // "sendByte"
+        QT_MOC_LITERAL(270, 4),  // "Mode"
+        QT_MOC_LITERAL(275, 7),  // "Address"
+        QT_MOC_LITERAL(283, 4),  // "Data"
+        QT_MOC_LITERAL(288, 8),  // "getBytes"
+        QT_MOC_LITERAL(297, 8),  // "swapBits"
+        QT_MOC_LITERAL(306, 4),  // "temp"
+        QT_MOC_LITERAL(311, 3),  // "QRY"
+        QT_MOC_LITERAL(315, 10),  // "AppendWord"
+        QT_MOC_LITERAL(326, 10),  // "datastring"
+        QT_MOC_LITERAL(337, 5),  // "value"
+        QT_MOC_LITERAL(343, 28),  // "populateComboBoxWithALGFiles"
+        QT_MOC_LITERAL(372, 10),  // "folderPath"
+        QT_MOC_LITERAL(383, 11),  // "readALGFile"
+        QT_MOC_LITERAL(395, 8),  // "filePath"
+        QT_MOC_LITERAL(404, 8),  // "ALGData&"
+        QT_MOC_LITERAL(413, 7),  // "algData"
+        QT_MOC_LITERAL(421, 14),  // "InterrogateCFI"
+        QT_MOC_LITERAL(436, 12),  // "isCFIpresent"
+        QT_MOC_LITERAL(449, 24),  // "on_actionAbout_triggered"
+        QT_MOC_LITERAL(474, 27),  // "on_actionAbout_QT_triggered"
+        QT_MOC_LITERAL(502, 34),  // "on_pushButton_DownloadSave_cl..."
+        QT_MOC_LITERAL(537, 32),  // "on_pushButton_UploadSave_clicked"
+        QT_MOC_LITERAL(570, 28),  // "on_pushButton_Detect_clicked"
+        QT_MOC_LITERAL(599, 31),  // "on_pushButton_UploadROM_clicked"
+        QT_MOC_LITERAL(631, 25),  // "on_pushButton_RTC_clicked"
+        QT_MOC_LITERAL(657, 30),  // "on_pushButton_SRAMTEST_clicked"
+        QT_MOC_LITERAL(688, 45),  // "on_comboBox_FlashCartType_cur..."
+        QT_MOC_LITERAL(734, 5),  // "index"
+        QT_MOC_LITERAL(740, 21)   // "on_pushButton_clicked"
     },
     "MainWindow",
     "pollSerialPorts",
@@ -187,6 +205,10 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
     "MBC",
     "uint32_t",
     "offset",
+    "requestAndReceiveROMfast",
+    "ReadMode",
+    "baseAddress",
+    "bytesToRead",
     "requestAndReceiveRAM",
     "sendByte",
     "Mode",
@@ -213,6 +235,8 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
     "on_pushButton_UploadSave_clicked",
     "on_pushButton_Detect_clicked",
     "on_pushButton_UploadROM_clicked",
+    "on_pushButton_RTC_clicked",
+    "on_pushButton_SRAMTEST_clicked",
     "on_comboBox_FlashCartType_currentIndexChanged",
     "index",
     "on_pushButton_clicked"
@@ -227,7 +251,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-      25,   14, // methods
+      28,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -235,31 +259,34 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,  164,    2, 0x08,    1 /* Private */,
-       3,    0,  165,    2, 0x08,    2 /* Private */,
-       4,    0,  166,    2, 0x08,    3 /* Private */,
-       5,    0,  167,    2, 0x08,    4 /* Private */,
-       6,    0,  168,    2, 0x08,    5 /* Private */,
-       7,    0,  169,    2, 0x08,    6 /* Private */,
-       8,    2,  170,    2, 0x08,    7 /* Private */,
-      13,    2,  175,    2, 0x08,   10 /* Private */,
-      14,    3,  180,    2, 0x08,   13 /* Private */,
-      18,    2,  187,    2, 0x08,   17 /* Private */,
-      19,    1,  192,    2, 0x08,   20 /* Private */,
-      21,    2,  195,    2, 0x08,   22 /* Private */,
-      22,    2,  200,    2, 0x08,   25 /* Private */,
-      25,    1,  205,    2, 0x08,   28 /* Private */,
-      27,    2,  208,    2, 0x08,   30 /* Private */,
-      31,    0,  213,    2, 0x08,   33 /* Private */,
-      32,    0,  214,    2, 0x08,   34 /* Private */,
-      33,    0,  215,    2, 0x08,   35 /* Private */,
-      34,    0,  216,    2, 0x08,   36 /* Private */,
-      35,    0,  217,    2, 0x08,   37 /* Private */,
-      36,    0,  218,    2, 0x08,   38 /* Private */,
-      37,    0,  219,    2, 0x08,   39 /* Private */,
-      38,    0,  220,    2, 0x08,   40 /* Private */,
-      39,    1,  221,    2, 0x08,   41 /* Private */,
-      41,    0,  224,    2, 0x08,   43 /* Private */,
+       1,    0,  182,    2, 0x08,    1 /* Private */,
+       3,    0,  183,    2, 0x08,    2 /* Private */,
+       4,    0,  184,    2, 0x08,    3 /* Private */,
+       5,    0,  185,    2, 0x08,    4 /* Private */,
+       6,    0,  186,    2, 0x08,    5 /* Private */,
+       7,    0,  187,    2, 0x08,    6 /* Private */,
+       8,    2,  188,    2, 0x08,    7 /* Private */,
+      13,    3,  193,    2, 0x08,   10 /* Private */,
+      17,    2,  200,    2, 0x08,   14 /* Private */,
+      18,    3,  205,    2, 0x08,   17 /* Private */,
+      22,    2,  212,    2, 0x08,   21 /* Private */,
+      23,    1,  217,    2, 0x08,   24 /* Private */,
+      25,    2,  220,    2, 0x08,   26 /* Private */,
+      26,    2,  225,    2, 0x08,   29 /* Private */,
+      29,    1,  230,    2, 0x08,   32 /* Private */,
+      31,    2,  233,    2, 0x08,   34 /* Private */,
+      35,    0,  238,    2, 0x08,   37 /* Private */,
+      36,    0,  239,    2, 0x08,   38 /* Private */,
+      37,    0,  240,    2, 0x08,   39 /* Private */,
+      38,    0,  241,    2, 0x08,   40 /* Private */,
+      39,    0,  242,    2, 0x08,   41 /* Private */,
+      40,    0,  243,    2, 0x08,   42 /* Private */,
+      41,    0,  244,    2, 0x08,   43 /* Private */,
+      42,    0,  245,    2, 0x08,   44 /* Private */,
+      43,    0,  246,    2, 0x08,   45 /* Private */,
+      44,    0,  247,    2, 0x08,   46 /* Private */,
+      45,    1,  248,    2, 0x08,   47 /* Private */,
+      47,    0,  251,    2, 0x08,   49 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -269,14 +296,15 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, 0x80000000 | 9, 0x80000000 | 11,   10,   12,
+    QMetaType::Void, 0x80000000 | 9, 0x80000000 | 11, 0x80000000 | 11,   14,   15,   16,
     QMetaType::Void, 0x80000000 | 9, 0x80000000 | 11,   10,   12,
-    QMetaType::Void, QMetaType::Char, QMetaType::UInt, QMetaType::UInt,   15,   16,   17,
-    QMetaType::QByteArray, QMetaType::Char, QMetaType::UInt,   15,   16,
-    QMetaType::QByteArray, QMetaType::QByteArray,   20,
-    QMetaType::UChar, QMetaType::UInt, QMetaType::UInt,   16,   17,
-    QMetaType::QByteArray, QMetaType::QByteArray, QMetaType::UInt,   23,   24,
-    QMetaType::Void, QMetaType::QString,   26,
-    QMetaType::Bool, QMetaType::QString, 0x80000000 | 29,   28,   30,
+    QMetaType::Void, QMetaType::Char, QMetaType::UInt, QMetaType::UInt,   19,   20,   21,
+    QMetaType::QByteArray, QMetaType::Char, QMetaType::UInt,   19,   20,
+    QMetaType::QByteArray, QMetaType::QByteArray,   24,
+    QMetaType::UChar, QMetaType::UInt, QMetaType::UInt,   20,   21,
+    QMetaType::QByteArray, QMetaType::QByteArray, QMetaType::UInt,   27,   28,
+    QMetaType::Void, QMetaType::QString,   30,
+    QMetaType::Bool, QMetaType::QString, 0x80000000 | 33,   32,   34,
     QMetaType::Void,
     QMetaType::UInt,
     QMetaType::Void,
@@ -285,7 +313,9 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   40,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   46,
     QMetaType::Void,
 
        0        // eod
@@ -315,6 +345,11 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         // method 'requestAndReceiveROM'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<uint8_t, std::false_type>,
+        QtPrivate::TypeAndForceComplete<uint32_t, std::false_type>,
+        // method 'requestAndReceiveROMfast'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<uint8_t, std::false_type>,
+        QtPrivate::TypeAndForceComplete<uint32_t, std::false_type>,
         QtPrivate::TypeAndForceComplete<uint32_t, std::false_type>,
         // method 'requestAndReceiveRAM'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
@@ -363,6 +398,10 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_pushButton_UploadROM_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_RTC_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_SRAMTEST_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_comboBox_FlashCartType_currentIndexChanged'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<int, std::false_type>,
@@ -385,30 +424,33 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 4: _t->FetchCartData(); break;
         case 5: _t->Init(); break;
         case 6: _t->requestAndReceiveROM((*reinterpret_cast< std::add_pointer_t<uint8_t>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint32_t>>(_a[2]))); break;
-        case 7: _t->requestAndReceiveRAM((*reinterpret_cast< std::add_pointer_t<uint8_t>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint32_t>>(_a[2]))); break;
-        case 8: _t->sendByte((*reinterpret_cast< std::add_pointer_t<char>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[3]))); break;
-        case 9: { QByteArray _r = _t->getBytes((*reinterpret_cast< std::add_pointer_t<char>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
+        case 7: _t->requestAndReceiveROMfast((*reinterpret_cast< std::add_pointer_t<uint8_t>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint32_t>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<uint32_t>>(_a[3]))); break;
+        case 8: _t->requestAndReceiveRAM((*reinterpret_cast< std::add_pointer_t<uint8_t>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint32_t>>(_a[2]))); break;
+        case 9: _t->sendByte((*reinterpret_cast< std::add_pointer_t<char>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[3]))); break;
+        case 10: { QByteArray _r = _t->getBytes((*reinterpret_cast< std::add_pointer_t<char>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
             if (_a[0]) *reinterpret_cast< QByteArray*>(_a[0]) = std::move(_r); }  break;
-        case 10: { QByteArray _r = _t->swapBits((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1])));
+        case 11: { QByteArray _r = _t->swapBits((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1])));
             if (_a[0]) *reinterpret_cast< QByteArray*>(_a[0]) = std::move(_r); }  break;
-        case 11: { uchar _r = _t->QRY((*reinterpret_cast< std::add_pointer_t<uint>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
+        case 12: { uchar _r = _t->QRY((*reinterpret_cast< std::add_pointer_t<uint>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
             if (_a[0]) *reinterpret_cast< uchar*>(_a[0]) = std::move(_r); }  break;
-        case 12: { QByteArray _r = _t->AppendWord((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
+        case 13: { QByteArray _r = _t->AppendWord((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<uint>>(_a[2])));
             if (_a[0]) *reinterpret_cast< QByteArray*>(_a[0]) = std::move(_r); }  break;
-        case 13: _t->populateComboBoxWithALGFiles((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
-        case 14: { bool _r = _t->readALGFile((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<ALGData&>>(_a[2])));
+        case 14: _t->populateComboBoxWithALGFiles((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 15: { bool _r = _t->readALGFile((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<ALGData&>>(_a[2])));
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
-        case 15: _t->InterrogateCFI(); break;
-        case 16: { uint _r = _t->isCFIpresent();
+        case 16: _t->InterrogateCFI(); break;
+        case 17: { uint _r = _t->isCFIpresent();
             if (_a[0]) *reinterpret_cast< uint*>(_a[0]) = std::move(_r); }  break;
-        case 17: _t->on_actionAbout_triggered(); break;
-        case 18: _t->on_actionAbout_QT_triggered(); break;
-        case 19: _t->on_pushButton_DownloadSave_clicked(); break;
-        case 20: _t->on_pushButton_UploadSave_clicked(); break;
-        case 21: _t->on_pushButton_Detect_clicked(); break;
-        case 22: _t->on_pushButton_UploadROM_clicked(); break;
-        case 23: _t->on_comboBox_FlashCartType_currentIndexChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
-        case 24: _t->on_pushButton_clicked(); break;
+        case 18: _t->on_actionAbout_triggered(); break;
+        case 19: _t->on_actionAbout_QT_triggered(); break;
+        case 20: _t->on_pushButton_DownloadSave_clicked(); break;
+        case 21: _t->on_pushButton_UploadSave_clicked(); break;
+        case 22: _t->on_pushButton_Detect_clicked(); break;
+        case 23: _t->on_pushButton_UploadROM_clicked(); break;
+        case 24: _t->on_pushButton_RTC_clicked(); break;
+        case 25: _t->on_pushButton_SRAMTEST_clicked(); break;
+        case 26: _t->on_comboBox_FlashCartType_currentIndexChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 27: _t->on_pushButton_clicked(); break;
         default: ;
         }
     }
@@ -433,13 +475,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 25)
+        if (_id < 28)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 25;
+        _id -= 28;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 25)
+        if (_id < 28)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 25;
+        _id -= 28;
     }
     return _id;
 }
---
+++ Win-x64-binary/readme.txt
@@ -0,0 +1,12 @@
+JoeyGUI uses the Qt Toolkit under the terms of the GNU Lesser General Public License v. 3 and the GNU Lesser General Public License v. 2.1 (both jointly "LGPL"). In compliance with LGPL, this document collects the relevant information about downloading, installing, and building the Qt Toolkit from sources. The main source of the information and most of the web links provided here come from the Qt Company's website (http://www.qt.io/)
+LGPL License Text
+The first step in compliance with LGPL is that every recipient of the JoeyGUI software receives also the full text of the LGPL licenses used. JoeyGUI complies with this requirement by including the full text of the LGPL licenses (lgpl-2.1.txt and lgpl-3.txt) in every JoeyGUI distribution. The full text of the LGPL licenses can be found in both the release folder and root source folder.
+On each supported platform (Windows, Linux, and macOS), JoeyGUI dynamically links to the unmodified Qt libraries, as provided by the Qt Company in the pre-compiled binary format.
+Also, in compliance with LGPL, the "About QT" dialog box of the JoeyGUI application prominently states that this program uses the Qt Toolkit under the terms of LGPL.
+Qt Version Information
+Additionally, the JoeyGUI 'About Qt' Menu includes the standard "About Qt" dialog box, which informs the user about the exact version of Qt and explains the Qt licensing terms. Finally, the QM Manual prominently states that this program uses the Qt Toolkit under the terms of LGPL.
+Qt Library Source Code
+JoeyGUI has used Qt version 6.6.1. You can download the Qt 6.6.1 source here: 
+Qt 6.6.1 https://download.qt.io/archive/qt/6.6/6.6.1/single/
+Written Offer to Provide Source Code
+In case you have any trouble downloading the Qt Source Code from the official link provided above, BennVenn Electronics makes hereby a written offer to provide such source code to you. To make a Qt source code request, send email to the following address: support@bennvenn.com. Qt version 6.6.1 will be available for three (3) years after the release date of the corresponding QM tool.
